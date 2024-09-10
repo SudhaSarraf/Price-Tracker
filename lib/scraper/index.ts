c1d1d41adf79f7e6830cb18615467abd08643321
sudhasarraf <sudhasarraf217@gmail.com>
2024-09-10 07:58:14 +0545

    scraping product name, current price and original price

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
@@ -23,7 +24,26 @@ export async function scrapeAmazonProduct(url: string) {
     try {
         // Fetch the product page
         const response = await axios.get(url, options);
-        console.log(response.data);
+        const $ = cheerio.load(response.data);
+
+        //Extract the product title
+        const title = $('#productTitle').text().trim();
+        const currentPrice = extractPrice(
+            $('.priceToPay span.a-price-whole'),
+            $('a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+            $('.a-price.a-text-price.a-size-medium.apexPriceToPay span.a-offscreen')
+        );
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('.a-price.a-text-price.a-size-base span.a-offscreen'), //.com
+            $('.a-price.a-text-price span.a-offscreen'), //.in
+            $('#listPrice'),
+            $('#priceblock_dealprice'),
+            $('.a-size-base.a-color-price')
+        )
+        
+        console.log({title, currentPrice, originalPrice});
     } catch (error: any) {
         throw new Error(`Failed to scrape product: ${error.message}`)
     }
