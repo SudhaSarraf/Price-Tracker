39c1ba97c25c09f97410ed289fc6109803c953b1
sudhasarraf <sudhasarraf217@gmail.com>
2024-09-18 21:15:38 +0545

    working on data extraction

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,3 +6,8 @@ export function extractPrice(...elements: any)  {
     }
     return '';
 }
+
+export function extractCurrency(element: any) {
+    const currentText = element.text().trim().slice(0, 1);
+    return currentText ? currentText : '';
+}
